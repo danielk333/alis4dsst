dc2df6160a47efdbfb38107483d997f37abfcb9b
danielk <daniel.kastinen@irf.se>
2022-02-17 17:50:55 +0100

    experimenting with aida tools and image analysis

--- curved_line_in_image.py
+++ curved_line_in_image.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 from pathlib import Path
 import argparse
+import re
 
 from astropy.io import fits
 from astropy.time import Time, TimeDelta
 from scipy.signal import convolve2d
 from scipy.optimize import minimize
+from scipy.io import loadmat
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import numpy as np
+from tqdm import tqdm
 
 import sorts
 import pyant
 import pyorb
+
+try:
+    import aida
+    import aida.camera
+except ImportError:
+    aida = None
 
 knt_geo = dict(
     lat=67+51/60+20.7/3600,
@@ -28,6 +38,15 @@ prop = sorts.propagator.Kepler(
     ),
 )
 
+
+def read_optpar(file):
+    fname = str(file.resolve())
+    mat_data = loadmat(fname)
+    optmod_pattern = r'_m[0-9]_'
+    optmod_match = re.search(optmod_pattern, file.name)
+    optmod = int(optmod_match[0][2:-1]) if optmod_match else None
+    return mat_data['optpar'].flatten(), optmod
+    
 
 def find_sat_trace(in_image, set_value=10, mean_threshold=4):
     """
@@ -110,6 +129,23 @@ def read_ALIS4D(file):
     return ALIS_im, fits_h
     
 
+def get_Camera(optpar, optmod):
+    camera = aida.camera.Camera(
+        horizontalFocalLength=optpar[0],
+        verticalFocalLength=optpar[1],
+        rotationAngleAlpha=optpar[2],
+        rotationAngleBeta=optpar[3],
+        rotationAngleGamma=optpar[4],
+        horizontalDisplacement=optpar[5],
+        verticalDisplacement=optpar[6],
+        pinholeDeviation=optpar[7],
+    )
+    if optmod == 3:
+        camera.optical_transfer = aida.camera.model.alis3d
+        camera.inverse_optical_transfer = aida.camera.invmodel.inv_alis3d
+    return camera
+
+
 def generate_trace_data(path, cache=False, clobber=False, **kwargs):
 
     path = Path(path)
@@ -134,37 +170,34 @@ def generate_trace_data(path, cache=False, clobber=False, **kwargs):
     return data_dict, fits_h
 
 
-def locate_in_center(x, orb, st, pointing, epoch):
-    orb0 = orb.copy()
-    orb0.i = x[0]
-    orb0.Omega = x[1]
-    orb0.anom = x[2]
+def angle_to_pointing(orb, st, pointing, epoch):
     pos = sorts.frames.convert(
         epoch, 
-        orb0.cartesian, 
+        orb.cartesian, 
         in_frame='TEME', 
         out_frame='ITRS',
     )
-    pos = pos.flatten()[:3]
+    pos = pos[:3, :]
 
     local = st.enu(pos)
-    ang = pyant.coordinates.vector_angle(local, pointing)
+    ang = pyant.coordinates.vector_angle(pointing, local)
     return ang
 
 
-def curve(t, orb, epoch):
+def curve(t, orb, epoch, camera):
 
     # THIS WILL TAKE ORBIT AND CALCULATE PIXELS IN IMAGE WITH SIZE (using pixels + psf)
     ecef = prop.propagate(t, orb, epoch=epoch)
 
     local_cart = knt_st.enu(ecef[:3, :])
-    local_sph = pyant.coordinates.cart_to_sph(local_cart)
+    local_sph = pyant.coordinates.cart_to_sph(local_cart, radians=True)
+    local_sph[1, :] = np.pi/2 - local_sph[1, :]
 
-    pixels = magic_function(local_sph)
+    u, v = camera.project_directions(local_sph[0, :], local_sph[1, :])
 
-    pixels =' widen stuff with psf'
+    # TODO: ADD PSF HERE
 
-    return pixels
+    return np.stack([u, v], axis=1)
 
 
 def curve_to_index(points, data_index, shape):
@@ -178,11 +211,10 @@ def curve_to_index(points, data_index, shape):
     return inds
 
 
-def curve_transform(orb, stuff):
-
-    # CALCULATE THE GENERALIZED CURVE TRANSFORM
-
-    xy = curve(orb, data)
+def curve_transform(img, index, t, orb, epoch, camera, shape):
+    xy = curve(t, orb, epoch, camera)
+    xy[0, :] *= shape[0]
+    xy[1, :] *= shape[1]
     xy_ind = curve_to_index(xy, index, shape)
     val = np.sum(img[xy_ind])
     return val
@@ -217,108 +249,138 @@ def detect_trace(args):
     A = data_dict['A'].copy()
     A[A > 10] = 10
 
-    for key in fits_h:
-        print(f'{key} = {fits_h[key]}')
-    exit()
+    file = Path(args.optpar)
+    optpar, optmod = read_optpar(file)
 
-    framerate = float(fits_h['REXPSTR'].replace('Hz', ''))
-    t_interval = fits_h['NAXIS3']/framerate
+    assert optmod == 3, f'huh, optmod={optmod}'
+
+    t_exp_start = np.arange(fits_h['NAXIS3'])*fits_h['CCDTKIN']
+    t_exp_end = t_exp_start + fits_h['EXPTIME']
+    exp_samples = 20
+
+    t = np.hstack([
+        np.linspace(t_exp_start[ind], t_exp_end[ind], exp_samples)
+        for ind in range(fits_h['NAXIS3'])
+    ])
     epoch = Time(fits_h['DATE-OBS'], format='isot', scale='utc')
-    point_az = float(fits_h['AZIMUTH'])
-    point_el = 90.0 - float(fits_h['ZENITANG'])
 
     if fits_h['SITENAME'].lower().strip() == 'kirunaknutstorp':
         st = knt_st
     else:
-        assert 0, 'wtf'
+        assert 0, f'warning unauthorized teleportation \
+            detected to {fits_h["SITENAME"]}, error error'
+
+    sph_pointing = np.array([
+        float(fits_h['AZIMUTH']),
+        90.0 - float(fits_h['ZENITANG']),
+        1
+    ])
+    pointing = pyant.coordinates.sph_to_cart(sph_pointing, radians=True)
+
+    camera = get_Camera(optpar, optmod)
 
     shape = A.shape
     A = A.reshape(A.size)
-    data_index = np.arange(A.size).reshape(*shape)
+    A_index = np.arange(A.size).reshape(*shape)
 
-    num = int(np.sqrt(shape[0]**2 + shape[1]**2)) + 1
+    # orb = pyorb.Orbit(
+    #     M0=pyorb.M_earth, m=0, 
+    #     num=1, epoch=epoch, 
+    #     radians=False,
+    # )
+    # orb.a = 7e6
+    # orb.omega = 0
+
+    # a_samp = np.linspace(6.7e6, 10e6, num=20)
+    inc_samp = np.arange(60, 150, 1, dtype=np.float64)
+    Omega_samp = np.arange(0, 360, 2.5, dtype=np.float64)
+    nu_samp = np.arange(0, 360, 2.5, dtype=np.float64)
+    inc, Omega, nu = np.meshgrid(
+        inc_samp,
+        Omega_samp,
+        nu_samp,
+        indexing='ij',
+    )
+    samples = inc.size
+    samples_shape = inc.shape
+
+    print(samples, samples_shape)
+
+    inc = np.reshape(inc, (samples, ))
+    Omega = np.reshape(Omega, (samples, ))
+    nu = np.reshape(nu, (samples, ))
 
     orb = pyorb.Orbit(
-        M0=pyorb.M_earth, m=0, 
-        num=1, epoch=epoch, 
+        M0=pyorb.M_earth, 
+        m=0, 
+        num=samples, 
+        epoch=epoch, 
         radians=False,
+        auto_update = False,
+        direct_update = False,
     )
     orb.a = 7e6
     orb.e = 0
+    orb.i = inc
     orb.omega = 0
+    orb.Omega = Omega
+    orb.anom = nu
+    orb.calculate_cartesian()
 
-    it = 1
-    x0 = np.array([51, 79, 158])
-    min_res = minimize(
-        locate_in_center, 
-        x0, 
-        args=(orb, st, pointing, epoch), 
-        method='Nelder-Mead',
+    # ang = angle_to_pointing(orb, st, pointing, epoch)
+
+    # ang = np.reshape(ang, samples_shape)
+    # inc = np.reshape(inc, samples_shape)
+    # Omega = np.reshape(Omega, samples_shape)
+    # nu = np.reshape(nu, samples_shape)
+
+    # fig, ax = plt.subplots()
+    # pm = ax.pcolormesh(inc[:, :, 0], Omega[:, :, 0], ang[:, :, 0])
+    # cbar = fig.colorbar(pm, ax=ax)
+    # pm.set_clim(0, 180)
+
+    # def update(frame):
+    #     ax.set_title(f'nu={nu_samp[frame]}')
+    #     pm.set_array(ang[:, :, frame].flatten())
+    #     return pm,
+
+    # ani = FuncAnimation(
+    #     fig, update, 
+    #     frames=range(samples_shape[2]), 
+    #     blit=False,
+    # )
+    # plt.show()
+
+    # fig, ax = plt.subplots()
+    # pm = ax.pcolormesh(inc[:, :, 0], Omega[:, :, 0], ang[:, :, 0])
+    # fig.colorbar(pm, ax=ax)
+    # plt.show()
+    # exit()
+
+    ecef = sorts.frames.convert(
+        epoch, 
+        orb.cartesian, 
+        in_frame='TEME', 
+        out_frame='ITRS',
     )
-    if args.v:
-        print(f'{it}: [{min_res.fun}] {x0}')
-    while it < 10 and min_res.fun > 0.1:
-        it += 1
-        x0 = np.random.rand(3)
-        x0[0] *= 90
-        x0[1:] *= 360
-        min_res0 = minimize(
-            locate_in_center, 
-            x0, 
-            args=(orb, st, pointing, epoch), 
-            method='Nelder-Mead',
-        )
+    local_cart = knt_st.enu(ecef[:3, :])
+    local_sph = pyant.coordinates.cart_to_sph(local_cart, radians=True)
+    local_sph[1, :] = np.pi/2 - local_sph[1, :]
 
-        if args.v:
-            print(f'{it}: [{min_res0.fun}] {x0} -> {min_res0.x}')
-        if min_res0.fun < min_res.fun:
-            min_res = min_res0
-        if min_res.fun < 0.1:
-            break
-    if args.v:
-        print(min_res)
+    u, v = camera.project_directions(local_sph[0, :], local_sph[1, :])
+    keep = np.logical_and(u > 0, u < 1)
+    keep = np.logical_and(
+        keep,
+        np.logical_and(v > 0, v < 1),
+    )
+    u = u[keep]
+    v = v[keep]
+    xy = np.stack([u*shape[0], v*shape[1]], axis=1)
 
-    orb.i = min_res.x[0]
-    orb.Omega = min_res.x[1]
-    orb.anom = min_res.x[2]
-
-    # Now this orb is at center of pointing at epoch
-
-    # time extent of track from fits data
-    t = stuff
-
-
-    def opt_function(p):
-        return -curve_transform(p, num, A, data_index, shape)
-
-    np.random.seed(args.seed)
-
-    best_result = None
-    for ind in range(1000):
-        x0 = [
-            np.random.randint(shape[0]),
-            np.random.randint(shape[1]),
-            np.random.randint(shape[0]),
-            np.random.randint(shape[1]),
-            np.random.randint(shape[0]),
-            np.random.randint(shape[1]),
-        ]
-        result = minimize(opt_function, x0, method='Nelder-Mead')
-        if best_result is None:
-            best_result = result
-        
-        if result.fun < best_result.fun:
-            best_result = result
-
-        print(f'iter={ind} -> best {best_result.fun} [current {result.fun}]')
-
-    print(best_result)
-
-    t = np.linspace(0, 1, num=num)
-    xy = curve(t, *best_result.x)
+    print(xy.shape[1])
 
     fig, ax = plt.subplots()
-    ax.plot(xy[0, :], xy[1, :], '-r')
+    ax.plot(xy[0, :], xy[1, :], '.r')
     ax.matshow(
         data_dict['A'], 
         cmap='gray', norm=None, 
@@ -326,34 +388,80 @@ def detect_trace(args):
     )
     plt.show()
 
+    # # orb0.i = orb.i + p[0]
+    # # orb0.anom = orb.anom + p[1]
+    # val = curve_transform(A, A_index, t, orb, epoch, camera, shape)
+    # print(f'Curve transform val = {val}')
+
+    # xy = curve(t, orb, epoch, camera)
+    # xy[0, :] *= shape[0]
+    # xy[1, :] *= shape[1]
+
+    # fig, ax = plt.subplots()
+    # ax.plot(xy[0, :], xy[1, :], '-r')
+    # ax.matshow(
+    #     data_dict['A'], 
+    #     cmap='gray', norm=None, 
+    #     vmax=10, origin='lower',
+    # )
+    # plt.show()
+
+
+def print_fits(args):
+    get_unit_regex = r'\[.*\]'
+    header = fits.getheader(Path(args.path))
+    for key in header:
+        comment = header.comments[key]
+        unit_match = re.search(get_unit_regex, comment)
+        if unit_match:
+            unit = unit_match[0][1:-1]
+        else:
+            unit = ''
+        print(f'{key} = {header[key]} [{unit}] \\\\{comment}')
+
 
 def main(input_args=None):
     parser = argparse.ArgumentParser(
         description='Analyse ALIS4D images for satellite tracks')
-    parser.add_argument(
-        'path', 
-        type=str,
-        help='Path to the fits image to plot',
-    )
-    parser.add_argument(
-        '-c', '--cache', 
-        action='store_true',
-        help='Cache results data along the way',
-    )
-    parser.add_argument(
-        '-C', '--clobber', 
-        action='store_true',
-        help='Override the cache results',
-    )
     subparsers = parser.add_subparsers(help='Action to perform', dest='command')
     plot_parser = subparsers.add_parser('plot', help='Plot the input image')
     det_parser = subparsers.add_parser('detect', help='Detect curve in image')
-    det_parser.add_argument(
-        '--seed', 
-        type=int,
-        default=837624764,
-        help='Numpy seed',
+    fits_parser = subparsers.add_parser('fits', help='Print FITS header')
+    opt_parser = subparsers.add_parser(
+        'optpar', 
+        help='Get Optical parameters from file',
     )
+
+    for _pars in [plot_parser, det_parser]:
+        _pars.add_argument(
+            '-c', '--cache', 
+            action='store_true',
+            help='Cache results data along the way',
+        )
+        _pars.add_argument(
+            '-C', '--clobber', 
+            action='store_true',
+            help='Override the cache results',
+        )
+        _pars.add_argument(
+            '--seed', 
+            type=int,
+            default=837624764,
+            help='Numpy seed',
+        )
+    for _pars in [fits_parser, plot_parser, det_parser]:
+        _pars.add_argument(
+            'path', 
+            type=str,
+            help='Path to the fits image to plot',
+        )
+
+    for _pars in [det_parser, opt_parser]:
+        _pars.add_argument(
+            'optpar', 
+            type=str,
+            help='Path to the optical parameter file',
+        )
 
     if input_args is None:
         args = parser.parse_args()
@@ -364,6 +472,14 @@ def main(input_args=None):
         plot_data(args)
     elif args.command == 'detect':
         detect_trace(args)
+    elif args.command == 'fits':
+        print_fits(args)
+    elif args.command == 'optpar':
+        file = Path(args.optpar)
+        optpar, optmod = read_optpar(file)
+        print('Optpar = ')
+        print(optpar)
+        print(f'Optmod = {optmod}')
 
 
 if __name__ == '__main__':
